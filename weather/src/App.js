ca6dfa8a7024951cd7ca2de4248e6407b67b07d9
Oslier <48390709+Oslier@users.noreply.github.com>
2021-01-05 01:54:37 -0500

    Basic frame and CSS

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,8 +1,25 @@
 import './App.css';
+import sun from './sun.png';
+import sky from './sky.jpg';
 
 function App() {
   return (
     <div className="App">
+
+      <div className="container">
+
+        <input type="text" className="searchBar"/>
+
+        <img src={sky} alt="" className="sky"/>
+
+        <div className="mainFrame">
+
+          <img src={sun} alt="" className="sun"/>
+
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis, modi.
+
+        </div>
+      </div>
 
     </div>
   );
