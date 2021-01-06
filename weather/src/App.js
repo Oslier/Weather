dbdc5d2c9428696bdebfd65fb6013d759fc4cc21
Oslier <48390709+Oslier@users.noreply.github.com>
2021-01-06 03:43:12 -0500

    Implemented API

--- weather/src/App.js
+++ weather/src/App.js
@@ -3,6 +3,23 @@ import sun from './sun.png';
 import sky from './sky.jpg';
 
 function App() {
+
+  let cityName = "Miami";
+
+  let temp;
+
+  const key = "509e28c5ef0b935055aa3d7630ccec35";
+  const api = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}`
+
+  fetch(api)
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      console.log(data.main.temp);
+      temp = data.main.temp;
+    })
+
   return (
     <div className="App">
 
@@ -16,7 +33,7 @@ function App() {
 
           <img src={sun} alt="" className="sun"/>
 
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis, modi.
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis, modi. {temp}
 
         </div>
       </div>
